3ce43b7ca8a1252f493fa3b225c94c8ee32764d7
Piero Nolte <pieronolte@gmail.com>
2023-10-08 13:28:50 -0500

    se agrega  sessionStorage.clear()

--- JS/main.js
+++ JS/main.js
@@ -19,59 +19,8 @@ let AllProducts = [];
 let Cart = [];
 
 // BASE DE DATOS DE PRODUCTOS LOCALSOTRAG
-// productos = JSON.parse(localStorage.getItem("Productos"));
-// ProductosPromociones = JSON.parse(localStorage.getItem("Promociones"));
-
-productos.push(new Producto("Ron", 750, "El abuelo", 32.00, 24));
-productos.push(new Producto("Ron", 750, "Zacapa 12 años", 42.00, 20));
-productos.push(new Producto("Ron", 750, "Flor de caña", 34.00, 22));
-
-productos.push(new Producto("Ron", 750, "Zacapa XO", 60.00, 30));
-productos.push(new Producto("Ron", 1750, "Flor de caña", 62.00, 18));
-productos.push(new Producto("Ron", 1750, "Barcelo", 82.00, 14));
-productos.push(new Producto("Whisky", 1000, "JW Red label", 22.00, 24));
-productos.push(new Producto("Whisky", 750, "JW Black label", 22.00, 24));
-productos.push(new Producto("Whisky", 750, "JW Double Black", 22.00, 24));
-productos.push(new Producto("Whisky", 750, "JW Gold label", 22.00, 24));
-productos.push(new Producto("Pisco", 750, "SQ Quebranta", 22.00, 24));
-productos.push(new Producto("Pisco", 2000, "SQ Quebranta", 22.00, 24));
-productos.push(new Producto("Pisco", 750, "Cuatro Gallos", 22.00, 24));
-productos.push(new Producto("Pisco", 750, "Intipalka", 22.00, 24));
-productos.push(new Producto("Vino", 750, "Tabernero Blanco", 18.00, 32));
-productos.push(new Producto("Vino", 750, "Tabernero Rosé", 22.00, 32));
-productos.push(new Producto("Vino", 750, "Tabernero Malbec", 24.00, 32));
-productos.push(new Producto("Vino", 1500, "Tabernero Borgoña", 24.00, 32));
-productos.push(new Producto("Gin", 700, "Beefeater", 62.00, 18));
-productos.push(new Producto("Gin", 700, "Beefeater pink", 72.00, 12));
-productos.push(new Producto("Gin", 700, "Beefeater orange", 68.00, 22));
-productos.push(new Producto("Bebida", 375, "Red Bull", 22.00, 50));
-productos.push(new Producto("Bebida", 375, "Red Bull Light", 22.00, 50));
-productos.push(new Producto("Bebida", 1500, "Schweppes", 22.00, 52));
-productos.push(new Producto("Bebida", 1500, "Schweppes Citrus", 22.00, 52));
-productos.push(new Producto("Bebida", 500, "Coca Cola", 22.00, 78));
-productos.push(new Producto("Bebida", 500, "Coca Cola Zero", 22.00, 72))
-productos.push(new Producto("Bebida", 2500, "Coca Cola", 22.00, 45));
-productos.push(new Producto("Bebida", 1500, "Coca Cola Zero", 22.00, 28));
-productos.push(new Producto("Bebida", 2500, "Coca Cola Zero", 22.00, 39));
-productos.push(new Producto("Hielo", 3000, "Bells", 15.00, 24));
-productos.push(new Producto("Hielo", 3000, "Artisan", 6.00, 24));
-productos.push(new Producto("Hielo", 1800, "Bells Cokctail", 10.00, 24));
-
-
-localStorage.setItem("Productos", JSON.stringify(productos));
-
-// BASE DE DATOS PROMOCIONES
-ProductosPromociones.push(new Producto("Bebida", 500, "Coca Cola 6unid", 42.00, 20));
-ProductosPromociones.push(new Producto("Bebida", 3000, "Coca Cola 2unid", 42.00, 20));
-ProductosPromociones.push(new Producto("Vino", 750, "Tabernero Borgoña 2unid", 34.00, 22));
-ProductosPromociones.push(new Producto("Pisco", 750, "Cuatro Gallos 2unid", 60.00, 30));
-ProductosPromociones.push(new Producto("Whisky", 750, "JW Black label 2unid", 62.00, 18));
-ProductosPromociones.push(new Producto("Whisky", 750, "JW Black label + 6unid Pilsen", 82.00, 14));
-ProductosPromociones.push(new Producto("Whisky", 750, "JW Red label + Schweppes", 22.00, 24));
-ProductosPromociones.push(new Producto("Bebida", 1500, "Red bull 6unid", 22.00, 24));
-ProductosPromociones.push(new Producto("Bebida", 1500, "Red bull Light 6unid", 22.00, 24));
-
-localStorage.setItem("Promociones", JSON.stringify(ProductosPromociones));
+productos = JSON.parse(localStorage.getItem("Productos"));
+ProductosPromociones = JSON.parse(localStorage.getItem("Promociones"));
 
 // BASE DE DATOS DE PRODUCTOS POR FILTRO
 let ProductoRon = productos.filter((el) => { return el.nombre == "Ron" });
@@ -251,11 +200,12 @@ productList.addEventListener(`click`, e => {
             AllProducts = [...AllProducts, InfoCart];
             Cart = [...Cart, NewItem];
         }
-
+        sessionStorage.clear();
         console.log(Cart);
         console.log(AllProducts);
         ContainerCarrito.innerHTML = "";
         ShowCart();
+        
     }
     sessionStorage.setItem("Cart", JSON.stringify(Cart));
 }

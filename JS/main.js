c0bf24a7879300bef45e5c73807e04c92ed48a86
Piero Nolte <pieronolte@gmail.com>
2023-10-11 14:56:45 -0500

    f5

--- JS/main.js
+++ JS/main.js
@@ -25,7 +25,7 @@ let ProductosJSOn = [];
 
 fetch('./productos.json')
   .then((response) =>{
-    if(response.ok){
+    if(!response.ok){
         return response.json();
     }
     console.log("Hola");

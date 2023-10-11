28f1127539aa6610a0df083e38a5778e260536dc
Piero Nolte <pieronolte@gmail.com>
2023-10-11 14:50:09 -0500

    f5

--- JS/main.js
+++ JS/main.js
@@ -23,11 +23,7 @@ let ProductosJSOn = [];
 
 // FUNCION FETCH PARA BASE DE DATOS
 
-fetch('./productos.json', {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
+fetch('./productos.json')
   .then((response) =>{
     if(response.ok){
         return response.json();
